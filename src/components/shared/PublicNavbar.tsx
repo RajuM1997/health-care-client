b788f2a788b0bd500ea2e6cd5688f6e2d78111cc
RajuM1997 <mraju5797@gmail.com>
2025-12-25 17:06:25 +0600

    part-1 done

---
+++ src/components/shared/PublicNavbar.tsx
@@ -0,0 +1,95 @@
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { Menu } from "lucide-react";
+
+const PublicNavbar = () => {
+  const navItems = [
+    { name: "Home", href: "/" },
+    { name: "Consultation", href: "/consultation" },
+    { name: "Health Plans", href: "/health-plans" },
+    { name: "Diagnostics", href: "/diagnostics" },
+    { name: "NGOs", href: "/ngos" },
+  ];
+  return (
+    <header className="sticky to-0 z-50 border-b w-full bg-background/95 px-4 backdrop-blur dark:bg-background/95">
+      <div className="container mx-auto flex h-16 items-center justify-between px-4">
+        <div>
+          <Link
+            href={"/"}
+            className="flex items-center justify-center text-xl font-bold text-primary"
+          >
+            PH-Health
+          </Link>
+        </div>
+        <nav className="hidden md:block">
+          <ul className="flex gap-6">
+            {navItems.map((item) => (
+              <li key={item.name}>
+                <Link
+                  href={item.href}
+                  className="text-sm font-medium text-muted-foreground hover:text-primary"
+                >
+                  {item.name}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+        <div className="hidden md:block">
+          <Link href={"/"}>
+            <Button>Login</Button>
+          </Link>
+        </div>
+        {/* mobile menu */}
+        <div className="md:hidden">
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button variant="outline">
+                <Menu />
+              </Button>
+            </SheetTrigger>
+            <SheetContent>
+              <SheetHeader>
+                <SheetTitle>Edit profile</SheetTitle>
+                <SheetDescription>
+                  Make changes to your profile here. Click save when you&apos;re
+                  done.
+                </SheetDescription>
+              </SheetHeader>
+              <div className="grid flex-1 auto-rows-min gap-6 px-4">
+                <div className="grid gap-3">
+                  <Label htmlFor="sheet-demo-name">Name</Label>
+                  <Input id="sheet-demo-name" defaultValue="Pedro Duarte" />
+                </div>
+                <div className="grid gap-3">
+                  <Label htmlFor="sheet-demo-username">Username</Label>
+                  <Input id="sheet-demo-username" defaultValue="@peduarte" />
+                </div>
+              </div>
+              <SheetFooter>
+                <Button type="submit">Save changes</Button>
+                <SheetClose asChild>
+                  <Button variant="outline">Close</Button>
+                </SheetClose>
+              </SheetFooter>
+            </SheetContent>
+          </Sheet>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default PublicNavbar;
